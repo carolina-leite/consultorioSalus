5b8abd9ca7517642473c9373559483176df82e5d
carolina-leite <araujoanacarolina@yahoo.com.br>
2021-06-20 23:02:47 -0400

    alteraçoes localização

---
+++ app/src/main/java/com/example/consultoriosalus/AnimacaoInicial.java
@@ -0,0 +1,13 @@
+package com.example.consultoriosalus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnimacaoInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animacao_inicial);
+    }
+}
