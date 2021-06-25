cc84981120e11120f804678eb0406b0e21059238
carolina-leite <araujoanacarolina@yahoo.com.br>
2021-06-24 23:31:21 -0400

    tela cadastro do paciente

---
+++ app/src/main/java/com/example/consultoriosalus/CadastroPaciente.java
@@ -0,0 +1,14 @@
+package com.example.consultoriosalus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroPaciente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_paciente);
+    }
+}
